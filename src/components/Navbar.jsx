6a2f96a62f13776de4c81cc8e60cba2e81b15ad4
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-07-07 18:43:54 +0200

    Update to Layout, Nav, and Pages structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <Link href="/" className="text-white font-bold">
+              Logo
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                href="/game"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Game
+              </Link>
+              <Link
+                href="/player"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Player
+              </Link>
+              <Link
+                href="/unused"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Unused
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
