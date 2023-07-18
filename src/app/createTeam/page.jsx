7bcaca2874ed77b006ea10a2344e084d1546b3e0
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-07-18 18:07:15 +0200

    Courts POST/Model implemented

--- src/app/createTeam/page.jsx
+++ src/app/createTeam/page.jsx
@@ -14,7 +14,6 @@ const CreateTeam = () => {
     leagues: [],
   });
 
-  const [filteredPlayers, setFilteredPlayers] = useState([]);
   const [existingPlayers, setExistingPlayers] = useState([]);
   const [existingCourts, setExistingCourts] = useState([]);
 
