a1f56e043f1ca0e67452ebf8578579fbd12dabc9
Gereon Czerny <gereon.czerny@yahoo.com>
2023-07-28 22:08:34 +0200

    aaaaaaaaaaylmao

--- src/app/adminPanel/page.jsx
+++ src/app/adminPanel/page.jsx
@@ -59,25 +59,25 @@ function AdminPage() {
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="mb-5">
         <button
-          className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2"
+          className="px-4 py-2 btn-primary  text-neutral  rounded-md mr-2"
           onClick={handleLeagueToggle}
         >
           {showLeagues ? "Hide League Creation" : "Create League"}
         </button>
         <button
-          className="px-4 py-2 bg-green-500 text-white rounded-md mr-2"
+          className="px-4 py-2 btn-primary  text-neutral rounded-md mr-2"
           onClick={handleTeamsToggle}
         >
           {showTeams ? "Hide Teams" : "Show Teams"}
         </button>
         <button
-          className="px-4 py-2 bg-yellow-500 text-white rounded-md mr-2"
+          className="px-4 py-2 btn-primary  text-neutral rounded-md mr-2"
           onClick={handlePlayersToggle}
         >
           {showPlayers ? "Hide Players" : "Show Players"}
         </button>
         <button
-          className="px-4 py-2 bg-red-500 text-white rounded-md"
+          className="px-4 py-2 btn-primary  text-neutral rounded-md"
           onClick={handlePlayersCreateToggle}
         >
           {createPlayers ? "Hide Player Creation" : "Create a Player"}
